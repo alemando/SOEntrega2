ffcc7ca4ba55603bf1ffee186bc561d0f6a926dc
Alejandro <35750771+alemando@users.noreply.github.com>
2020-03-01 18:04:57 -0500

    avances

--- ip/script.js
+++ ip/script.js
@@ -4,15 +4,22 @@ $(document).ready(function(){
         var ip1 = $("#ip1").val();
         var ip2 = $("#ip2").val();
         var red = $("#red").val();
-        console.log(ip1)
-        console.log(ip2)
-        console.log(red)
-        /*Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Something went wrong!',
-            footer: '<a href>Why do I have this issue?</a>'
-          })*/
+        var regex = RegExp("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$");
+        if(regex.test(ip1) && regex.test(ip2)){
+            var sip1 = ip1.split(".");
+            var sip2 = ip2.split(".");
+
+            console.log(parseInt(sip1[0]).toString(2))
+            console.log(ip2)
+        }else if(regex.test(ip1) && regex.test(red)){
+            console.log(red)
+        }else{
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Mal formato en la Dirección IP o Mascara de red\n Ejemplo : 168.1.10.1'
+              })
+        }
       });
   
   });
